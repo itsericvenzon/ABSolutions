febc3b4088451a857de60358f6cadf4af38bf63e
itsericvenzon <eric.venzon@lethbridgecollege.ca>
2018-03-16 08:35:24 -0600

    new logo

---
+++ new/assets/js/selectdropdown.js
@@ -0,0 +1,5 @@
+$('.dropdown-menu').on( 'click', 'a', function() {
+    var text = $(this).html();
+    var htmlText = text + ' <span class="caret"></span>';
+    $(this).closest('.dropdown').find('.dropdown-toggle').html(htmlText);
+});
