41493f24b37aab2f7747f8c05a662e3ea1a77dd5
itsericvenzonbackup <flshbcksofr3d@yahoo.com>
2018-03-20 05:08:09 -0600

    commit changes

--- assets/js/u-popover.js
+++ assets/js/u-popover.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('[data-toggle="popover"]').popover({
         placement : 'top',
         html : true,
-        title : 'Username',
+        title : 'Username' '<a href="#" class="close" data-dismiss="alert">&times;</a>'
         content : '<div class="media"> <a href="#" class="pull-left"></a><div class="media-body"><p><table class="popovertext"><tr><td>Age:</td><td>#age</td></tr><td>Country:</td><td>#country</td></tr><td>Education:</td><td>#education</td></tr><td>Gender:</td><td>#gender</td></tr></table><hr/><button type="button" class="btn btn-warning btn-sm">Logout</button> &nbsp <a role="button" class="btn btn-dark btn-sm" href="u-deactivate.html">Deactivate Account </a></p></div></div>'
     });
 });
